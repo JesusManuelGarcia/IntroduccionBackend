8b46932d77a39a2661be7b8535caf6f6ba346ce9
JesúsManuelGarcia <jesus-manuel-gar2000@hotmail.com>
2022-04-27 11:26:26 -0600

    Se modifico linea de codigo con html

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
-const http = require("http");
-const hostname = "192.168.50.176";
+const http = require('http');
+const hostname = '192.168.50.176';
 const port = 3000;
 const server = http.createServer((req, res)=>{
-res.statuscode=200;
-res.setheader("content-type", "text/html");
-res.end("Hola Mundo\n");
+res.statusCode=200;
+res.setHeader('Content-Type', 'text/html');
+res.end('Hola Mundo\n');
 });
-
-server.listen(port, hostname, ()=>{
-console.log("el servidor se esta ejecutando en http://192.168.50.176:3000/");
-});
+server.listen(port, hostname,function () {
+        console.log('el servidor se esta ejecutando en http://192.168.50.176:3000/');
+    });
