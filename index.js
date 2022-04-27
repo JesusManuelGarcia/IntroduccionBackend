7d32f238998d47a69534e91d7778caa320bd5af7
JesúsManuelGarcia <jesus-manuel-gar2000@hotmail.com>
2022-04-27 10:59:30 -0600

    Se modifico a html

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const hostname = "127.0.0.1";
 const port = 3000;
 const server = http.createServer((req, res)=>{
 res.statuscode=200;
-res.setheader("content-type", "text/plain");
+res.setheader("content-type", "text/html");
 res.end("Hola Mundo\n");
 });
 
